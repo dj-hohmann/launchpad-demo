8ae4fd886f140527c72011a2e47c5a9f7257a5a9
unknown <Matthew.White@1433-W7E.moneysupermarket.com>
2014-10-08 21:11:06 +0100

    Jenkins plugin duplicate class issue fix

--- LaunchPadDemo/JenkinsPlugin.cs
+++ LaunchPadDemo/JenkinsPlugin.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace LaunchPadDemo
 {
-    class JenkinsPlugin
+    public class JenkinsPlugin : BaseLaunchpadPlugin
     {
+        private DateTime dt = DateTime.Now;
+
+        public override void Action()
+        {
+            System.Diagnostics.Process.Start("chrome.exe");
+        }
+
+        public override void Poll()
+        {
+            if (dt.AddSeconds(5) < DateTime.Now)
+            {
+                this.status = LaunchPadStatus.ALERT;
+            }
+        }
     }
 }
+
